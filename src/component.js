2f8da4b77b955a0a8cc2df1179fd529354e3528d
chattrb0x <vv.junk@protonmail.com>
2025-10-10 17:33:05 -0700

    :fire: remove unused attributes on Position

--- src/component.js
+++ src/component.js
@@ -21,9 +21,6 @@ class Position extends Component {
   constructor(v = new Vector2(0, 0)) {
     super()
     this.vector = v
-    this.x = 0
-    this.y = 0
-    this.screenIndex = 0
   }
 }
 class Velocity extends Component {
